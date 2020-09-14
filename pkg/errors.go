ade286e8c256c0805f725e13ef10805926575faf
Link Dupont <link@sub-pop.net>
2020-09-14 13:35:11 -0400

    pkg: add error sentinels for known errors
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- pkg/errors.go
+++ pkg/errors.go
@@ -5,14 +5,27 @@ import (
 	"net/http"
 )
 
+// ErrInvalidContentType indicates an unsupported "collector" value was given
+// in the upload request.
+var ErrInvalidContentType = &APIResponseError{
+	Code: http.StatusUnsupportedMediaType,
+	body: "Content type of payload is unsupported",
+}
+
+// ErrPayloadTooLarge indicates an upload request body exceeded the size limit.
+var ErrPayloadTooLarge = &APIResponseError{
+	Code: http.StatusRequestEntityTooLarge,
+	body: "Payload too large",
+}
+
 // An APIResponseError represents an unexpected response from an HTTP method call.
 type APIResponseError struct {
-	code int
+	Code int
 	body string
 }
 
 func (e APIResponseError) Error() string {
-	v := fmt.Sprintf("unexpected response: %v - %v", e.code, http.StatusText(e.code))
+	v := fmt.Sprintf("unexpected response: %v - %v", e.Code, http.StatusText(e.Code))
 	if e.body != "" {
 		v += fmt.Sprintf(" (%v)", e.body)
 	}
