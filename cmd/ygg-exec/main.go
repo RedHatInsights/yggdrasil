51feb8b57c19fe3896ab9055765ffb474542817d
Link Dupont <link@sub-pop.net>
2020-09-15 11:32:58 -0400

    abstract newApp into internal
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- cmd/ygg-exec/main.go
+++ cmd/ygg-exec/main.go
@@ -8,59 +8,13 @@ import (
 	internal "github.com/redhatinsights/yggdrasil/internal"
 	yggdrasil "github.com/redhatinsights/yggdrasil/pkg"
 	"github.com/urfave/cli/v2"
-	"github.com/urfave/cli/v2/altsrc"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Version = yggdrasil.Version
-
-	defaultConfigFilePath, err := internal.ConfigPath()
+	app, err := internal.NewApp("ygg-exec")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-
-	app.Flags = []cli.Flag{
-		&cli.StringFlag{
-			Name:      "config",
-			Value:     defaultConfigFilePath,
-			TakesFile: true,
-		},
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "base-url",
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "auth-mode",
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "username",
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "password",
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "cert-file",
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "key-file",
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
-			Name: "ca-root",
-		}),
-	}
-
-	// This BeforeFunc will load flag values from a config file only if the
-	// "config" flag value is non-zero.
-	app.Before = func(c *cli.Context) error {
-		if c.String("config") != "" {
-			inputSource, err := altsrc.NewTomlSourceFromFlagFunc("config")(c)
-			if err != nil {
-				return err
-			}
-			return altsrc.ApplyInputSourceValues(c, inputSource, app.Flags)
-		}
-		return nil
 	}
 
 	app.Commands = []*cli.Command{
---
+++ internal/cli.go
@@ -0,0 +1,63 @@
+package yggdrasil
+
+import (
+	yggdrasil "github.com/redhatinsights/yggdrasil/pkg"
+	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
+)
+
+// NewApp creates a new cli Application with name and default flags.
+func NewApp(name string) (*cli.App, error) {
+	app := cli.NewApp()
+	app.Name = name
+	app.Version = yggdrasil.Version
+
+	defaultConfigFilePath, err := ConfigPath()
+	if err != nil {
+		return nil, err
+	}
+
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:      "config",
+			Value:     defaultConfigFilePath,
+			TakesFile: true,
+		},
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "base-url",
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "auth-mode",
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "username",
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "password",
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "cert-file",
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "key-file",
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name: "ca-root",
+		}),
+	}
+
+	// This BeforeFunc will load flag values from a config file only if the
+	// "config" flag value is non-zero.
+	app.Before = func(c *cli.Context) error {
+		if c.String("config") != "" {
+			inputSource, err := altsrc.NewTomlSourceFromFlagFunc("config")(c)
+			if err != nil {
+				return err
+			}
+			return altsrc.ApplyInputSourceValues(c, inputSource, app.Flags)
+		}
+		return nil
+	}
+
+	return app, nil
+}
