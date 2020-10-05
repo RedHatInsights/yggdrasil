90399372be8d43cd7212fb688790dcec5201a80a
Link Dupont <link@sub-pop.net>
2020-10-05 12:32:14 -0400

    tests: test unmarshalling canoncal facts

--- canonical_facts_test.go
+++ canonical_facts_test.go
@@ -1,6 +1,7 @@
 package yggdrasil
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -63,6 +64,53 @@ func TestCanonicalFactsFromMap(t *testing.T) {
 				}
 				if !cmp.Equal(got, test.want) {
 					t.Errorf("diff got test.want\n--- got\n+++ test.want\n%v", cmp.Diff(got, test.want))
+				}
+			}
+		})
+	}
+}
+
+func TestCanonicalFactsUnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		description string
+		input       []byte
+		want        CanonicalFacts
+		wantError   error
+	}{
+		{
+			description: "empty",
+			input:       []byte(`{}`),
+			want:        CanonicalFacts{},
+		},
+		{
+			description: "valid",
+			input:       []byte(`{"insights_id":"bb69cd34-263f-444c-9278-5935b61d7f60","machine_id":"acc046d0-0add-4550-ac7c-5a833b1b6470","bios_uuid":"d8ec3cd5-a6bc-4742-bd2f-32940da182b0","subscription_manager_id":"bc452b83-c4ee-4b80-91d8-98ff816b2440","ip_addresses":["1.2.3.4", "5.6.7.8"],"fqdn":"foo.bar.com","mac_addresses":["CC:D1:7A:44:6D:1B", "A7:03:90:D0:05:A7"]}`),
+			want: CanonicalFacts{
+				InsightsID:            "bb69cd34-263f-444c-9278-5935b61d7f60",
+				MachineID:             "acc046d0-0add-4550-ac7c-5a833b1b6470",
+				BIOSUUID:              "d8ec3cd5-a6bc-4742-bd2f-32940da182b0",
+				SubscriptionManagerID: "bc452b83-c4ee-4b80-91d8-98ff816b2440",
+				IPAddresses:           []string{"1.2.3.4", "5.6.7.8"},
+				FQDN:                  "foo.bar.com",
+				MACAddresses:          []string{"CC:D1:7A:44:6D:1B", "A7:03:90:D0:05:A7"},
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			var got CanonicalFacts
+			err := json.Unmarshal(test.input, &got)
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError) {
+					t.Errorf("%v != %v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(got, test.want) {
+					t.Errorf("%v != %v", got, test.want)
 				}
 			}
 		})
