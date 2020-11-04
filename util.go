f789f8acf4fc6c477c50f8ccee1e9fbaa7cbf1cc
Link Dupont <link@sub-pop.net>
2020-11-04 14:31:03 -0500

    util: don't use UserConfigDir
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- util.go
+++ util.go
@@ -8,11 +8,7 @@ import (
 // ConfigPath returns an appropriate path to a config file. If the created path
 // does not exist, an empty string is returned.
 func ConfigPath() (string, error) {
-	prefix, err := os.UserConfigDir()
-	if err != nil {
-		return "", err
-	}
-	filePath := filepath.Join(prefix, LongName, "config.toml")
+	filePath := filepath.Join(SysconfDir, LongName, "config.toml")
 
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		return "", nil
