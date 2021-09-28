ffe17523db4458b96bdc83aee0003ee2fee2d73e
Link Dupont <link@sub-pop.net>
2021-09-27 21:15:41 -0400

    refactor: remove debug command
    Move the "generate" command to the top-level and make it hidden.
    
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- cmd/yggctl/main.go
+++ cmd/yggctl/main.go
@@ -21,93 +21,87 @@ func main() {
 
 	app.Commands = []*cli.Command{
 		{
-			Name:   "debug",
-			Usage:  "Utilities for debugging " + yggdrasil.LongName,
+			Name:   "generate",
+			Usage:  `Generate messages for publishing to client "in" topics.`,
 			Hidden: true,
 			Subcommands: []*cli.Command{
 				{
-					Name:  "generate",
-					Usage: `Generate messages for publishing to client "in" topics.`,
-					Subcommands: []*cli.Command{
-						{
-							Name:    "data-message",
-							Usage:   "Generate a data message.",
-							Aliases: []string{"data"},
-							Flags: []cli.Flag{
-								&cli.IntFlag{
-									Name:    "version",
-									Aliases: []string{"v"},
-									Value:   1,
-									Usage:   "set version to `NUM`",
-								},
-								&cli.StringFlag{
-									Name:    "response-to",
-									Aliases: []string{"r"},
-									Usage:   "reply to message `UUID`",
-								},
-								&cli.StringFlag{
-									Name:    "metadata",
-									Aliases: []string{"m"},
-									Value:   "{}",
-									Usage:   "set metadata to `JSON`",
-								},
-								&cli.StringFlag{
-									Name:     "directive",
-									Aliases:  []string{"d"},
-									Required: true,
-									Usage:    "set directive to `STRING`",
-								},
-							},
-							Action: func(c *cli.Context) error {
-								var metadata map[string]string
-								if err := json.Unmarshal([]byte(c.String("metadata")), &metadata); err != nil {
-									return cli.Exit(fmt.Errorf("cannot unmarshal metadata: %w", err), 1)
-								}
-
-								data, err := generateMessage("data", c.String("response-to"), c.String("directive"), c.Args().First(), metadata, c.Int("version"))
-								if err != nil {
-									return cli.Exit(fmt.Errorf("cannot marshal message: %w", err), 1)
-								}
-
-								fmt.Println(string(data))
-
-								return nil
-							},
+					Name:    "data-message",
+					Usage:   "Generate a data message.",
+					Aliases: []string{"data"},
+					Flags: []cli.Flag{
+						&cli.IntFlag{
+							Name:    "version",
+							Aliases: []string{"v"},
+							Value:   1,
+							Usage:   "set version to `NUM`",
 						},
-						{
-							Name:    "control-message",
-							Usage:   "Generate a control message.",
-							Aliases: []string{"control"},
-							Flags: []cli.Flag{
-								&cli.IntFlag{
-									Name:    "version",
-									Aliases: []string{"v"},
-									Value:   1,
-									Usage:   "set version to `NUM`",
-								},
-								&cli.StringFlag{
-									Name:    "response-to",
-									Aliases: []string{"r"},
-									Usage:   "reply to message `UUID`",
-								},
-								&cli.StringFlag{
-									Name:     "type",
-									Aliases:  []string{"t"},
-									Required: true,
-									Usage:    "set message type to `STRING`",
-								},
-							},
-							Action: func(c *cli.Context) error {
-								data, err := generateMessage(c.String("type"), c.String("response-to"), "", c.Args().First(), nil, c.Int("version"))
-								if err != nil {
-									return cli.Exit(fmt.Errorf("cannot marshal message: %w", err), 1)
-								}
-
-								fmt.Println(string(data))
-
-								return nil
-							},
+						&cli.StringFlag{
+							Name:    "response-to",
+							Aliases: []string{"r"},
+							Usage:   "reply to message `UUID`",
 						},
+						&cli.StringFlag{
+							Name:    "metadata",
+							Aliases: []string{"m"},
+							Value:   "{}",
+							Usage:   "set metadata to `JSON`",
+						},
+						&cli.StringFlag{
+							Name:     "directive",
+							Aliases:  []string{"d"},
+							Required: true,
+							Usage:    "set directive to `STRING`",
+						},
+					},
+					Action: func(c *cli.Context) error {
+						var metadata map[string]string
+						if err := json.Unmarshal([]byte(c.String("metadata")), &metadata); err != nil {
+							return cli.Exit(fmt.Errorf("cannot unmarshal metadata: %w", err), 1)
+						}
+
+						data, err := generateMessage("data", c.String("response-to"), c.String("directive"), c.Args().First(), metadata, c.Int("version"))
+						if err != nil {
+							return cli.Exit(fmt.Errorf("cannot marshal message: %w", err), 1)
+						}
+
+						fmt.Println(string(data))
+
+						return nil
+					},
+				},
+				{
+					Name:    "control-message",
+					Usage:   "Generate a control message.",
+					Aliases: []string{"control"},
+					Flags: []cli.Flag{
+						&cli.IntFlag{
+							Name:    "version",
+							Aliases: []string{"v"},
+							Value:   1,
+							Usage:   "set version to `NUM`",
+						},
+						&cli.StringFlag{
+							Name:    "response-to",
+							Aliases: []string{"r"},
+							Usage:   "reply to message `UUID`",
+						},
+						&cli.StringFlag{
+							Name:     "type",
+							Aliases:  []string{"t"},
+							Required: true,
+							Usage:    "set message type to `STRING`",
+						},
+					},
+					Action: func(c *cli.Context) error {
+						data, err := generateMessage(c.String("type"), c.String("response-to"), "", c.Args().First(), nil, c.Int("version"))
+						if err != nil {
+							return cli.Exit(fmt.Errorf("cannot marshal message: %w", err), 1)
+						}
+
+						fmt.Println(string(data))
+
+						return nil
 					},
 				},
 			},
