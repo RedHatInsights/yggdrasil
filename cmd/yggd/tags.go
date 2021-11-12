077f832ee8b0b88fd1edb57c357ddb5361cf541d
Link Dupont <link@sub-pop.net>
2021-11-12 14:28:25 -0500

    fix: make malformed tags content not fatal
    Change the tag parsing function to log an error, but continue to publish
    the connection-status message with an empty tags map instead.
    
    Fixes: ESSNTL-994
    Fixes: ESSNTL-1128
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- cmd/yggd/tags.go
+++ cmd/yggd/tags.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"os"
 	"reflect"
 	"strconv"
 	"time"
@@ -56,5 +57,20 @@ func readTags(in io.Reader) (map[string]string, error) {
 		}
 	}
 
+	return tags, nil
+}
+
+// readTagsFile reads tag data from file.
+func readTagsFile(file string) (map[string]string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open '%v' for reading: %w", file, err)
+	}
+	defer f.Close()
+
+	tags, err := readTags(f)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read tags file: %w", err)
+	}
 	return tags, nil
 }
