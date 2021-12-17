a3278ed95ce57218c4815faa1d90d4d642f48d9c
Link Dupont <link@sub-pop.net>
2021-12-17 11:12:28 -0500

    refactor: rename 'bytes' parameter to 'content'
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- cmd/yggctl/generate.go
+++ cmd/yggctl/generate.go
@@ -9,13 +9,13 @@ import (
 	"github.com/redhatinsights/yggdrasil"
 )
 
-func generateDataMessage(messageType yggdrasil.MessageType, responseTo string, directive string, bytes []byte, metadata map[string]string, version int) (*yggdrasil.Data, error) {
+func generateDataMessage(messageType yggdrasil.MessageType, responseTo string, directive string, content []byte, metadata map[string]string, version int) (*yggdrasil.Data, error) {
 	var data interface{}
-	if err := json.Unmarshal(bytes, &data); err != nil {
+	if err := json.Unmarshal(content, &data); err != nil {
 		return nil, fmt.Errorf("cannot unmarshal content: %v", err)
 	}
 
-	content, err := json.Marshal(data)
+	body, err := json.Marshal(data)
 	if err != nil {
 		return nil, fmt.Errorf("cannot marshal data: %v", err)
 	}
@@ -28,7 +28,7 @@ func generateDataMessage(messageType yggdrasil.MessageType, responseTo string, d
 		Sent:       time.Now(),
 		Directive:  directive,
 		Metadata:   metadata,
-		Content:    content,
+		Content:    body,
 	}
 
 	return &msg, nil
@@ -36,10 +36,10 @@ func generateDataMessage(messageType yggdrasil.MessageType, responseTo string, d
 
 // generateControlMessage creates a control message of the appropriate type by
 // switching on the value of messageType.
-func generateControlMessage(messageType yggdrasil.MessageType, responseTo string, version int, bytes []byte) (*yggdrasil.Control, error) {
+func generateControlMessage(messageType yggdrasil.MessageType, responseTo string, version int, content []byte) (*yggdrasil.Control, error) {
 	switch messageType {
 	case yggdrasil.MessageTypeCommand:
-		msg, err := generateCommandMessage(messageType, responseTo, version, bytes)
+		msg, err := generateCommandMessage(messageType, responseTo, version, content)
 		if err != nil {
 			return nil, fmt.Errorf("cannot generate command message: %v", err)
 		}
@@ -53,7 +53,7 @@ func generateControlMessage(messageType yggdrasil.MessageType, responseTo string
 		}
 		return &ctrl, nil
 	case yggdrasil.MessageTypeEvent:
-		msg, err := generateEventMessage(messageType, responseTo, version, bytes)
+		msg, err := generateEventMessage(messageType, responseTo, version, content)
 		if err != nil {
 			return nil, fmt.Errorf("cannot generate event message: %v", err)
 		}
@@ -72,7 +72,7 @@ func generateControlMessage(messageType yggdrasil.MessageType, responseTo string
 }
 
 // generateCommandMessage unmarshals bytes into a command message.
-func generateCommandMessage(messageType yggdrasil.MessageType, responseTo string, version int, bytes []byte) (*yggdrasil.Command, error) {
+func generateCommandMessage(messageType yggdrasil.MessageType, responseTo string, version int, content []byte) (*yggdrasil.Command, error) {
 	msg := yggdrasil.Command{
 		Type:       messageType,
 		MessageID:  uuid.New().String(),
@@ -81,21 +81,21 @@ func generateCommandMessage(messageType yggdrasil.MessageType, responseTo string
 		Sent:       time.Now(),
 	}
 
-	if err := json.Unmarshal(bytes, &msg.Content); err != nil {
+	if err := json.Unmarshal(content, &msg.Content); err != nil {
 		return nil, fmt.Errorf("cannot unmarshal content: %v", err)
 	}
 
 	return &msg, nil
 }
 
-func generateEventMessage(messageType yggdrasil.MessageType, responseTo string, version int, bytes []byte) (*yggdrasil.Event, error) {
+func generateEventMessage(messageType yggdrasil.MessageType, responseTo string, version int, content []byte) (*yggdrasil.Event, error) {
 	msg := yggdrasil.Event{
 		Type:       messageType,
 		MessageID:  uuid.New().String(),
 		ResponseTo: responseTo,
 		Version:    version,
 		Sent:       time.Now(),
-		Content:    string(bytes),
+		Content:    string(content),
 	}
 
 	return &msg, nil
