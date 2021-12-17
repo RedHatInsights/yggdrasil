fc499b7ae91da10bd174ef7cf249786f5aff90ab
Link Dupont <subpop@users.noreply.github.com>
2021-12-17 11:17:33 -0500

    Improve the yggctl generate commands (#49)
    * feat: improve the yggctl generate commands
    
    yggctl generate commands use the yggdrasil message data types directly,
    rather than relying on maps and JSON marshalling. Input is unmashalled
    into a yggdrasil message of the appropriate type, which is then
    marshalled into JSON before printing to standard out.
    
    Signed-off-by: Link Dupont <link@sub-pop.net>

---
+++ cmd/yggctl/generate.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/redhatinsights/yggdrasil"
+)
+
+func generateDataMessage(messageType yggdrasil.MessageType, responseTo string, directive string, content []byte, metadata map[string]string, version int) (*yggdrasil.Data, error) {
+	var data interface{}
+	if err := json.Unmarshal(content, &data); err != nil {
+		return nil, fmt.Errorf("cannot unmarshal content: %v", err)
+	}
+
+	body, err := json.Marshal(data)
+	if err != nil {
+		return nil, fmt.Errorf("cannot marshal data: %v", err)
+	}
+
+	msg := yggdrasil.Data{
+		Type:       messageType,
+		MessageID:  uuid.New().String(),
+		ResponseTo: responseTo,
+		Version:    version,
+		Sent:       time.Now(),
+		Directive:  directive,
+		Metadata:   metadata,
+		Content:    body,
+	}
+
+	return &msg, nil
+}
+
+// generateControlMessage creates a control message of the appropriate type by
+// switching on the value of messageType.
+func generateControlMessage(messageType yggdrasil.MessageType, responseTo string, version int, content []byte) (*yggdrasil.Control, error) {
+	switch messageType {
+	case yggdrasil.MessageTypeCommand:
+		msg, err := generateCommandMessage(messageType, responseTo, version, content)
+		if err != nil {
+			return nil, fmt.Errorf("cannot generate command message: %v", err)
+		}
+		data, err := json.Marshal(msg)
+		if err != nil {
+			return nil, fmt.Errorf("cannot marshal command message: %v", err)
+		}
+		var ctrl yggdrasil.Control
+		if err := json.Unmarshal(data, &ctrl); err != nil {
+			return nil, fmt.Errorf("cannot unmarshal control message: %v", err)
+		}
+		return &ctrl, nil
+	case yggdrasil.MessageTypeEvent:
+		msg, err := generateEventMessage(messageType, responseTo, version, content)
+		if err != nil {
+			return nil, fmt.Errorf("cannot generate event message: %v", err)
+		}
+		data, err := json.Marshal(msg)
+		if err != nil {
+			return nil, fmt.Errorf("cannot marshal command message: %v", err)
+		}
+		var ctrl yggdrasil.Control
+		if err := json.Unmarshal(data, &ctrl); err != nil {
+			return nil, fmt.Errorf("cannot unmarshal control message: %v", err)
+		}
+		return &ctrl, nil
+	default:
+		return nil, fmt.Errorf("unsupported message type: %v", messageType)
+	}
+}
+
+// generateCommandMessage unmarshals bytes into a command message.
+func generateCommandMessage(messageType yggdrasil.MessageType, responseTo string, version int, content []byte) (*yggdrasil.Command, error) {
+	msg := yggdrasil.Command{
+		Type:       messageType,
+		MessageID:  uuid.New().String(),
+		ResponseTo: responseTo,
+		Version:    version,
+		Sent:       time.Now(),
+	}
+
+	if err := json.Unmarshal(content, &msg.Content); err != nil {
+		return nil, fmt.Errorf("cannot unmarshal content: %v", err)
+	}
+
+	return &msg, nil
+}
+
+func generateEventMessage(messageType yggdrasil.MessageType, responseTo string, version int, content []byte) (*yggdrasil.Event, error) {
+	msg := yggdrasil.Event{
+		Type:       messageType,
+		MessageID:  uuid.New().String(),
+		ResponseTo: responseTo,
+		Version:    version,
+		Sent:       time.Now(),
+		Content:    string(content),
+	}
+
+	return &msg, nil
+}
---
+++ cmd/yggctl/generate_test.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/redhatinsights/yggdrasil"
+)
+
+type Input struct {
+	messageType string
+	responseTo  string
+	directive   string
+	content     []byte
+	metadata    map[string]string
+	version     int
+}
+
+func TestGenerateDataMessage(t *testing.T) {
+	tests := []struct {
+		description string
+		input       Input
+		want        *yggdrasil.Data
+		wantError   error
+	}{
+		{
+			description: "data JSON content",
+			input: Input{
+				messageType: "data",
+				directive:   "dir",
+				content:     []byte(`{"field":"value"}`),
+				metadata:    map[string]string{},
+				version:     1,
+			},
+			want: &yggdrasil.Data{
+				Type:      yggdrasil.MessageTypeData,
+				Version:   1,
+				Directive: "dir",
+				Metadata:  map[string]string{},
+				Content:   []byte(`{"field":"value"}`),
+			},
+		},
+		{
+			description: "data string content",
+			input: Input{
+				messageType: "data",
+				directive:   "dir",
+				content:     []byte(`"hello world"`),
+				metadata:    map[string]string{},
+				version:     1,
+			},
+			want: &yggdrasil.Data{
+				Type:      yggdrasil.MessageTypeData,
+				Version:   1,
+				Directive: "dir",
+				Metadata:  map[string]string{},
+				Content:   []byte(`"hello world"`),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got, err := generateDataMessage(yggdrasil.MessageType(test.input.messageType), test.input.responseTo, test.input.directive, test.input.content, test.input.metadata, test.input.version)
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError, cmpopts.EquateErrors()) {
+					t.Errorf("%#v != %#v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(got, test.want, cmpopts.IgnoreFields(yggdrasil.Data{}, "MessageID", "Sent")) {
+					t.Errorf("%#v != %#v", got, test.want)
+				}
+			}
+		})
+	}
+}
+
+func TestGenerateCommandMessage(t *testing.T) {
+	tests := []struct {
+		description string
+		input       Input
+		want        *yggdrasil.Command
+		wantError   error
+	}{
+		{
+			description: "command",
+			input: Input{
+				messageType: string(yggdrasil.MessageTypeCommand),
+				content:     []byte(`{"command":"ping","arguments":{}}`),
+				version:     1,
+			},
+			want: &yggdrasil.Command{
+				Type:    yggdrasil.MessageTypeCommand,
+				Version: 1,
+				Content: struct {
+					Command   yggdrasil.CommandName "json:\"command\""
+					Arguments map[string]string     "json:\"arguments\""
+				}{
+					Command:   "ping",
+					Arguments: map[string]string{},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got, err := generateCommandMessage(yggdrasil.MessageType(test.input.messageType), test.input.responseTo, test.input.version, test.input.content)
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError, cmpopts.EquateErrors()) {
+					t.Errorf("%#v != %#v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(got, test.want, cmpopts.IgnoreFields(yggdrasil.Command{}, "MessageID", "Sent")) {
+					t.Errorf("%#v != %#v", got, test.want)
+				}
+			}
+		})
+	}
+}
+
+func TestGenerateControlMessage(t *testing.T) {
+	tests := []struct {
+		description string
+		input       Input
+		want        *yggdrasil.Control
+		wantError   error
+	}{
+		{
+			input: Input{
+				messageType: "event",
+				content:     []byte(`pong`),
+				version:     1,
+			},
+			want: &yggdrasil.Control{
+				Type:    yggdrasil.MessageTypeEvent,
+				Version: 1,
+				Content: json.RawMessage(`"pong"`),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			got, err := generateControlMessage(yggdrasil.MessageType(test.input.messageType), test.input.responseTo, test.input.version, test.input.content)
+
+			if test.wantError != nil {
+				if !cmp.Equal(err, test.wantError, cmpopts.EquateErrors()) {
+					t.Errorf("%#v != %#v", err, test.wantError)
+				}
+			} else {
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !cmp.Equal(got, test.want, cmpopts.IgnoreFields(yggdrasil.Control{}, "MessageID", "Sent")) {
+					t.Errorf("%#v != %#v", got, test.want)
+				}
+			}
+		})
+	}
+}
