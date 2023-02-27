f3123fb52d3a8a0085aa555d6a84e1a8b96625ab
Link Dupont <link@sub-pop.net>
2023-02-27 12:15:29 -0500

    feat: add 'yggctl dispatch' command
    Add a 'dispatch' subcommand to yggctl. This subcommand can dispatch data
    from a file or stdin to a worker running locally on the system.
    
    Signed-off-by: Link Dupont <link@sub-pop.net>

--- cmd/yggctl/main.go
+++ cmd/yggctl/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 
 	"git.sr.ht/~spc/go-log"
 
+	"github.com/godbus/dbus/v5"
+	"github.com/google/uuid"
 	"github.com/redhatinsights/yggdrasil"
 	"github.com/redhatinsights/yggdrasil/internal/constants"
 	"github.com/urfave/cli/v2"
 )
-
-var DeveloperBuild = true
 
 func main() {
 	app := cli.NewApp()
@@ -23,11 +24,11 @@ func main() {
 	app.Flags = []cli.Flag{
 		&cli.BoolFlag{
 			Name:   "generate-man-page",
-			Hidden: !DeveloperBuild,
+			Hidden: true,
 		},
 		&cli.BoolFlag{
 			Name:   "generate-markdown",
-			Hidden: !DeveloperBuild,
+			Hidden: true,
 		},
 	}
 
@@ -35,7 +36,7 @@ func main() {
 		{
 			Name:   "generate",
 			Usage:  `Generate messages for publishing to client "in" topics.`,
-			Hidden: !DeveloperBuild,
+			Hidden: true,
 			Subcommands: []*cli.Command{
 				{
 					Name:    "data-message",
@@ -118,6 +119,70 @@ func main() {
 				},
 			},
 		},
+		{
+			Name:        "dispatch",
+			Usage:       "Dispatch data to a worker locally",
+			UsageText:   "yggctl dispatch [command options] FILE",
+			Description: "The dispatch command reads FILE and sends its content to a yggdrasil worker running locally. If FILE is -, content is read from stdin.",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "worker",
+					Aliases:  []string{"w"},
+					Usage:    "Send data to `WORKER`",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:    "metadata",
+					Aliases: []string{"m"},
+					Usage:   "Attach `JSON` as metadata to the message",
+					Value:   "{}",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				var conn *dbus.Conn
+				var err error
+
+				if os.Getenv("DBUS_SESSION_BUS_ADDRESS") != "" {
+					conn, err = dbus.ConnectSessionBus()
+				} else {
+					conn, err = dbus.ConnectSystemBus()
+				}
+				if err != nil {
+					return cli.Exit(fmt.Errorf("cannot connect to bus: %w", err), 1)
+				}
+
+				var metadata map[string]string
+				if err := json.Unmarshal([]byte(c.String("metadata")), &metadata); err != nil {
+					return cli.Exit(fmt.Errorf("cannot unmarshal metadata: %w", err), 1)
+				}
+
+				var data []byte
+				var r io.Reader
+				if c.Args().First() == "-" {
+					r = os.Stdin
+				} else {
+					r, err = os.Open(c.Args().First())
+				}
+				if err != nil {
+					return cli.Exit(fmt.Errorf("cannot open file for reading: %w", err), 1)
+				}
+				data, err = io.ReadAll(r)
+				if err != nil {
+					return cli.Exit(fmt.Errorf("cannot read data: %w", err), 1)
+				}
+
+				id := uuid.New().String()
+
+				obj := conn.Object("com.redhat.Yggdrasil1", "/com/redhat/Yggdrasil1")
+				if err := obj.Call("com.redhat.Yggdrasil1.Dispatch", dbus.Flags(0), c.String("worker"), id, metadata, data).Store(); err != nil {
+					return cli.Exit(fmt.Errorf("cannot dispatch message: %w", err), 1)
+				}
+
+				fmt.Printf("Dispatched message %v to worker %v\n", id, c.String("worker"))
+
+				return nil
+			},
+		},
 	}
 
 	app.Action = func(c *cli.Context) error {
@@ -137,7 +202,7 @@ func main() {
 			return nil
 		}
 
-		return nil
+		return cli.ShowAppHelp(c)
 	}
 
 	if err := app.Run(os.Args); err != nil {
