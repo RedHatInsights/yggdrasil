b48c49908db6cac8ea772037e76ee03f801b091c
Link Dupont <link@sub-pop.net>
2023-08-01 12:29:39 -0400

    fix: handle Event signals gracefully
    Return errors when handling Event signals emitted by workers and handle
    the optional 'message' parameter gracefully.
    
    Signed-off-by: Link Dupont <link@sub-pop.net>

---
+++ internal/work/errors.go
@@ -0,0 +1,28 @@
+package work
+
+import "reflect"
+
+// typeConversionError represents a conversion error when converting one type
+// to  another.
+type typeConversionError struct {
+	p reflect.Value
+	t reflect.Type
+}
+
+func (e typeConversionError) Error() string {
+	return "cannot convert " + e.p.Type().Name() + " to " + e.t.Name()
+}
+
+func newUint32TypeConversionError(parameter interface{}) typeConversionError {
+	return typeConversionError{
+		p: reflect.ValueOf(parameter),
+		t: reflect.TypeOf(uint32(0)),
+	}
+}
+
+func newStringTypeConversionError(parameter interface{}) typeConversionError {
+	return typeConversionError{
+		p: reflect.ValueOf(parameter),
+		t: reflect.TypeOf(""),
+	}
+}
